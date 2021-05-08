1a5a99dc13f1fbd060c58b4fcf09db45e01528e9
Alexandre Boissière <alexgf27550@gmail.com>
2021-05-08 22:12:29 +0200

    Added explicit conversion into native_binary.cpp to avoid MSVC compiler conversions warnings.

--- Registers/native_binary.cpp
+++ Registers/native_binary.cpp
@@ -22,19 +22,22 @@ void b_log2(std::shared_ptr<void> reg, regs* registers, memory* unused_m) {
 	registries_def reg_id = *std::static_pointer_cast<registries_def>(reg);
 
 	registries_ptr_table ptr_table = registries_ptr_table(registers);
-	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(log2l((((reg_int<unsigned long long>*)ptr_table.access(reg_id)))->get()));
+	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(
+		(unsigned long long)log2l((long double)((((reg_int<unsigned long long>*)ptr_table.access(reg_id)))->get())));
 }
 void b_log10(std::shared_ptr<void> reg, regs* registers, memory* unused_m) {
 	registries_def reg_id = *std::static_pointer_cast<registries_def>(reg);
 
 	registries_ptr_table ptr_table = registries_ptr_table(registers);
-	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(log10l((((reg_int<unsigned long long>*)ptr_table.access(reg_id)))->get()));
+	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(
+		(unsigned long long)log10l((long double)((((reg_int<unsigned long long>*)ptr_table.access(reg_id)))->get())));
 }
 void b_log(std::shared_ptr<void> reg, regs* registers, memory* unused_m) {
 	registries_def reg_id = *std::static_pointer_cast<registries_def>(reg);
 
 	registries_ptr_table ptr_table = registries_ptr_table(registers);
-	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(logbl((((reg_int<unsigned long long>*)ptr_table.access(reg_id)))->get()));
+	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(
+		(unsigned long long)logbl((long double)((((reg_int<unsigned long long>*)ptr_table.access(reg_id)))->get())));
 }
 
 /* STACK status before calling:
@@ -103,9 +106,10 @@ void b_pow(std::shared_ptr<void> reg, regs* registers, memory* mem) {
 
 	unsigned long long saved_rax = registers->rax->get();
 	popMem(std::make_shared<registries_def>(registries_def::RAX), registers, mem);
-	unsigned long long pow = registers->rax->get();
+	unsigned long long _pow = registers->rax->get();
 	registers->rax->set(saved_rax);
 
 	registries_ptr_table ptr_table = registries_ptr_table(registers);
-	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(powl((((reg_int<unsigned long long>*)ptr_table.access(reg_id))->get()), pow));
+	((reg_int<unsigned long long>*)ptr_table.access(reg_id))->set(
+		(unsigned long long)powl((long double)(((reg_int<unsigned long long>*)ptr_table.access(reg_id))->get()), (long double)_pow));
 }
