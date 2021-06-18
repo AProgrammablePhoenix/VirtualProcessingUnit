1b80ef1fb62658a5a698a3133dfe94753ca9e2b8
Alexandre Boissière <alexgf27550@gmail.com>
2021-06-18 11:50:25 +0200

    Added utility file, containing multiple definitions of useful inline functions.

--- utility.h
+++ utility.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
+#include <tuple>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 	#define ISWIN 1
@@ -10,6 +12,8 @@
 		#include <stdio.h>
 	#endif
 #endif
+
+typedef std::tuple<size_t, size_t> arg_tuple;
 
 inline void ULLTOA(size_t value, unsigned char** output) {
 	*output = new unsigned char[sizeof(size_t)];
@@ -45,4 +49,47 @@ inline double ATOD(unsigned char* _array) {
 #endif
 
 	return ret;
+}
+
+template<typename Tin, typename Tout> inline void mp_memcpy(Tin& _in, Tout& _out) {
+#if defined(ISWIN)
+	memcpy_s(&_out, sizeof(Tout), &_in, sizeof(Tin));
+#else
+	std::memcpy(&_out, &_in, sizeof(&_out));
+#endif
+}
+template<typename Tin, typename Tout> inline void mp_memcpy(Tin& _in, Tout& _out, size_t count) {
+#if defined(ISWIN)
+	memcpy_s(&_out, count, &_in, count);
+#else
+	std::memcpy(&_out, &_in, count);
+#endif
+}
+template<> inline void mp_memcpy<size_t, unsigned char*>(size_t& _in, unsigned char*& _out) {
+#if defined(ISWIN)
+	memcpy_s(_out, sizeof(size_t), &_in, sizeof(size_t));
+#else
+	std::memcpy(_out, _in, sizeof(size_t));
+#endif
+}
+template<> inline void mp_memcpy<unsigned char*, size_t>(unsigned char*& _in, size_t& _out) {
+#if defined(ISWIN)
+	memcpy_s(&_out, sizeof(size_t), _in, sizeof(size_t));
+#else
+	std::memcpy(&_out, &_in, sizeof(size_t));
+#endif
+}
+template<typename Tin> inline void mp_memcpy(Tin& _in, unsigned char* _out, size_t count = sizeof(Tin)) {
+#if defined(ISWIN)
+	memcpy_s(_out, count, &_in, count);
+#else
+	std::memcpy(_out, &_in, count);
+#endif
+}
+template<typename Tout> inline void mp_memcpy(unsigned char* _in, Tout& _out, size_t count = sizeof(Tout)) {
+#if defined(ISWIN)
+	memcpy_s(&_out, count, _in, count);
+#else
+	std::memcpy(&_out, _in, count);
+#endif
 }
